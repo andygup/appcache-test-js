8915e2940fe505960971821362fc9e5f89192d57
andygup <agup@esri.com>
2014-03-19 13:53:30 -0600

    bolted in saveTilesLocally functionality

--- modules/tiles.js
+++ modules/tiles.js
@@ -7,12 +7,59 @@ define([
         return declare(null, {
 
             _map: null,
+            _btnGetTiles:null,
+            _wantToCancel: false,
+            globalState:{},
 
             constructor: function(map)
             {
                 this._map = map;
                 this._initOfflineTiles(map);
+                this._btnGetTiles = document.getElementById("btn-get-tiles");
             },
+
+            saveTilesLocally: function(evt){
+                if( globalState.downloadState == 'downloading')
+                {
+                    console.log("cancel!");
+                    _wantToCancel = true;
+                }
+                else
+                {
+                    var minLevel = 0;
+                    var maxLevel = 16;
+                    var extent = this._map.geometry.getExtent();
+                    var buffer = 1500; /* approx meters (webmercator units) */
+                    extent.xmin -= buffer; extent.ymin -= buffer;
+                    extent.xmax += buffer; extent.ymax += buffer;
+                    _wantToCancel = false;
+                    this._map.prepareForOffline(minLevel, maxLevel, extent, lang.hitch(self,self._reportProgress));
+                    globalState.downloadState = 'downloading';
+                }
+            },
+
+            _reportProgress: function(progress)
+            {
+                console.log("downloading tiles...");
+                var percent = Math.floor(progress.countNow / progress.countMax * 100);
+                this._btnGetTiles.value = 'Saving to phone ' + percent + "% - Tap to Cancel";
+
+                if( progress.finishedDownloading )
+                {
+                    if( progress.cancelRequested )
+                    {
+                        globalState.downloadState = 'cancelled';
+                        this._btnGetTiles.value = 'Get Tiles';
+                    }
+                    else
+                    {
+                        globalState.downloadState = 'downloaded';
+                        this._btnGetTiles.value = 'Delete Tiles';
+                    }
+                }
+                return _wantToCancel;
+            },
+
             _initOfflineTiles: function(map){
                 OfflineTilesEnabler.extend(map,function(success)
                 {
