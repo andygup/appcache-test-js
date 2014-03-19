0fed7f4003f0d79430538102d2e9ca2c5a6a5dd7
andygup <agup@esri.com>
2014-03-19 11:41:19 -0600

    added arcgis js api, index.html and test manifest files

---
+++ dojo/esri/dijit/geoenrichment/UserError.js
@@ -0,0 +1,3 @@
+//>>built
+define("esri/dijit/geoenrichment/UserError",[],function(){function a(a){this.name="UserError";this.message=a||""}a.prototype=Error.prototype;return a});
+//@ sourceMappingURL=UserError.js.map
---
+++ dojo/esri/tasks/StatisticDefinition.js
@@ -0,0 +1,4 @@
+//>>built
+define("esri/tasks/StatisticDefinition",["dojo/_base/declare","dojo/_base/lang","dojo/has","esri/kernel"],function(a,b,c,d){a=a(null,{declaredClass:"esri.tasks.StatisticDefinition",statisticType:null,onStatisticField:null,outStatisticFieldName:null,toJson:function(){var a={statisticType:this.statisticType,onStatisticField:this.onStatisticField};this.outStatisticFieldName&&(a.outStatisticFieldName=this.outStatisticFieldName);return a}});c("extend-esri")&&b.setObject("tasks.StatisticDefinition",a,d);
+return a});
+//@ sourceMappingURL=StatisticDefinition.js.map
